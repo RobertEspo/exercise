001bb4eb689ad03d545e0a9ca21a9fcbd708e26c
Robert E <60628906+RobertEspo@users.noreply.github.com>
2024-06-20 15:19:19 -0400

    viz updates
    Updated 02_most_frequent_partners.R to include new partner and bar graph.
    Added future goals.

--- scripts/02_viz_most_frequent_partners.R
+++ scripts/02_viz_most_frequent_partners.R
@@ -10,6 +10,8 @@ source(here::here("scripts", "00_libs.R"))
 
 # -----------------------------------------------------------------------------
 
+# load data and select cols
+# rename values from codes to partner names
 routine <- range_read('1H8w_d53ZHczs8-gAtDsIfAlJnH07GHnZUi0Leh_dyOQ') %>%
   select(
     day,
@@ -22,10 +24,18 @@ routine <- range_read('1H8w_d53ZHczs8-gAtDsIfAlJnH07GHnZUi0Leh_dyOQ') %>%
                             ifelse(partner == 2, "Jordan",
                                    ifelse(partner == 3, "Jon",
                                           ifelse(partner == 4, "Jon_and_Jordan", 
-                                                 ifelse(partner == 5, "Jon_and_Luis", NA)))))),
+                                                 ifelse(partner == 5, "Jon_and_Luis",
+                                                        ifelse(partner == 6, "Luis", NA))))))),
     day
   )
 
+# partner counts in df
 partner_counts <- routine %>%
   group_by(partner) %>%
   summarize(count = n())
+
+# bar plot partner counts
+partner_counts <- routine %>% ggplot(
+  aes(x = partner)
+) +
+  geom_bar()
